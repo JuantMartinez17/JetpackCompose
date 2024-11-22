8e15f5589e293741c56b3b9d3f170e3fa3f940e5
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-22 20:43:59 -0300

    Implemented modifier

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose.ui.theme.Jetpack_ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +29,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MyTexts(){
 
-        Column{
+        Column(modifier = Modifier.padding(4.dp)){
             MyText(text = "Hola mundo usando Jetpack")
             MyText(text = "bla bla bla")
         }
@@ -39,6 +41,7 @@ class MainActivity : ComponentActivity() {
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
             contentDescription = "Imagen de prueba"
         )
+
     }
 
     @Composable
