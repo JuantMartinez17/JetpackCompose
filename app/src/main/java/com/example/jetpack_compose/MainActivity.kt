364d2456ee2debc4fe3c7d31c4dab30b9aadc6c5
JuantMartinez17 <juantomasm0@gmail.com>
2024-11-22 20:41:52 -0300

    Added image rendering function

--- app/src/main/java/com/example/jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -10,6 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose.ui.theme.Jetpack_ComposeTheme
 
@@ -32,6 +34,14 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
+    fun MyImage(){
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "Imagen de prueba"
+        )
+    }
+
+    @Composable
     fun MyText(text: String){
         Text(text = text)
     }
@@ -40,6 +50,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PreviewComponent(){
         MyTexts()
+        MyImage()
     }
 }
 
